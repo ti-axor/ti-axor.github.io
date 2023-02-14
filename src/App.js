76ee6fd47d17d941607d3809d5a52c29c5b02d0d
tiAxor <ti@axor.com.br>
2023-02-14 17:31:04 -0300

    feat: primeiro commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import logoaxor from './assets/download.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logoaxor} className="App-logo" alt="logo" />
+        <p>
+          Site em construção.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
